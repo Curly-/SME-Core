4bc41f721173a6dbd44d409dfa92b00f23bfef5b
Sam Bazley <samb1999@hotmail.co.uk>
2016-06-09 19:39:11 +0100

    Reorganised keys to support ASCII, added keycodes

--- SME_keyboard.cpp
+++ SME_keyboard.cpp
@@ -1,5 +1,5 @@
 #include "SME_keyboard.h"
 
-bool SME::Keyboard::KeyStates [sizeof (SME::Keyboard::OSScancodeTable) / sizeof (SME::Keyboard::Key::SME_KEY_UNKNOWN)] {
+bool SME::Keyboard::KeyStates [sizeof (SME::Keyboard::OSScancodeTable) / sizeof (SME::Keyboard::Key::KEY_UNKNOWN)] {
     false
 };
